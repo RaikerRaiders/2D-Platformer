51f13f795f0f6de2cf531ea0c38be2835581732f
RaikerRaiders <raikerraiders@gmail.com>
2026-09-24 14:10:51 +0300

    commit message

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -112,6 +112,6 @@ public class Player : MonoBehaviour
 
     private void Die()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
 }
